0d12f8bd469baf80daad9d89a927bd6881fc8160
Maarten de Magician <maarten.snels@politie.nl>
2020-09-11 00:00:48 +0200

    Eerste versie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RandomWordComponent } from './component/randomWord/random-word.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { DisplayWordComponent } from './component/display-word/display-word.component';
+import { KeyboardComponent } from './component/keyboard/keyboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomWordComponent,
+    DisplayWordComponent,
+    KeyboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
